2529287d57ac76864645ef5d5ae52f7bdf00312d
Caroline Riesten <caroline.riesten@yh.nackademin.se>
2018-09-23 10:45:07 +0200

    input player WC

--- SecretGame_New/Game.cs
+++ SecretGame_New/Game.cs
@@ -9,5 +9,6 @@ namespace SecretGame_New
     public class Game
     {
         
+
     }
 }
--- SecretGame_New/Program.cs
+++ SecretGame_New/Program.cs
@@ -15,7 +15,29 @@ namespace SecretGame_New
             //var player = gameBuilder.Initialize();
 
             //player. sköta kommunikationen med spelaren.
+            Console.WriteLine("Vart vill du gå?");
+
+            string direction = Console.ReadLine();
+            string[] directions = direction.Split(' ');
+
+            foreach (string s in directions)
+            {
+                Console.WriteLine(s);
+            }
+
+            if (directions[0] == "move")
+            {
+                Console.WriteLine("Du vill gå ");
+            }
+
+            if (directions[1] == "right")
+            {
+                Console.WriteLine("Du vill gå till höger");
+            }
+            // Console.WriteLine(direction);
+
         }
+    
     }
 }
  
